a1a12427944bcf03a61fd230b359f519bb850196
Andymanes <101294232+Andymanes@users.noreply.github.com>
2022-06-07 13:45:14 -0400

    connected fron and back end

--- src/App.js
+++ src/App.js
@@ -4,37 +4,11 @@ import React from 'react';
 
 
 function App() {
-  constructor(props) {
-    super(props);
-    this.state = { apiResponse: "" };
-}
-
-callAPI() {
-    fetch("http://localhost:4000/test")
-        .then(res => res.text())
-        .then(res => this.setState({ apiResponse: res }));
-}
-
-componentWillMount() {
-    this.callAPI();
-}
+ 
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Checking connection</h1>
     </div>
   );
 }
