b9a63f70600fc271e73e014f1d663927312a9063
Andymanes <101294232+Andymanes@users.noreply.github.com>
2022-06-10 09:54:32 -0400

    house page setup

---
+++ src/components/House.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react'
+
+function House() {
+    let [house, getHouse] = useState([])
+    useEffect(() => {
+        fetch('http://localhost:4000/house')
+        .then(response => {
+            console.log('Response:', response)
+            return response.json()
+        })
+        .then(data => {
+            console.log('Data:', data)
+            getHouse(data.results[0].members)
+        })
+    },[])
+    console.log('House:', house)
+    if (!house) return <p>loading...</p>
+
+    return (
+        <>
+            <p>house</p>
+            <div className='houseList'>
+                {house ? house.map((houseRep, idx) => {
+                    return (
+                        <div key={idx}>
+                            <h3>{houseRep?.last_name}</h3>
+                        </div>
+                    )
+                }) : <h2>thinking</h2>}
+            </div>
+        
+        
+        </>
+    )
+
+}
+
+
+export default House
