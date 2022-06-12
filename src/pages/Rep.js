171f069fab243880c9aac16da29fc6f6ba3ee6d2
Andymanes <101294232+Andymanes@users.noreply.github.com>
2022-06-11 22:11:40 -0400

    getting close drilling into data on rep page

--- src/pages/Rep.js
+++ src/pages/Rep.js
@@ -6,6 +6,7 @@ import Header from '../components/Header'
 function Rep (props) {
     let { id } = useParams()
     let [rep, setRep] = useState({})
+    let [repRole, setRepRole] = useState()
 
     const getApiData = async () => {
         const URL = `https://api.propublica.org/congress/v1/members/${id}.json`
@@ -17,8 +18,9 @@ function Rep (props) {
         }
         const data = await fetch(URL, options)
         const result = await data.json()
-        console.log(result)
+        // console.log(result)
         setRep(result.results[0])
+        setRepRole(rep.roles[0])
         
     }
     useEffect(() => {
@@ -26,14 +28,13 @@ function Rep (props) {
         
     },[])
     console.log('Rep:', rep)
-    
+    console.log('repRole:', repRole)
     return (
         <>
             <Header></Header>
-            <div></div>
-            <div>{JSON.stringify(rep.first_name)}</div>
-            <div>{JSON.stringify(rep.last_name)}</div>
-            <div>{JSON.stringify(rep.current_party)}</div>
+            <div>{rep?.first_name} {rep.last_name}</div>
+            <div>state {repRole?.state}</div>
+            <div>Politial Party ({rep.current_party})</div>
         </>
     )
 }
